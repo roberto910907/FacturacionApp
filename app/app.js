3a9f8c81548fb8eee7a1d809fc638e57f79173d1
Roberto Rielo <roberto910907@gmail.com>
2019-11-04 13:21:45 -0500

    Initial project structure

---
+++ app/app.js
@@ -0,0 +1,12 @@
+import Vue from 'nativescript-vue';
+import Home from './components/Home';
+
+new Vue({
+  components: {
+    Home,
+  },
+  template: `
+        <Frame>
+            <Home />
+        </Frame>`,
+}).$start();
